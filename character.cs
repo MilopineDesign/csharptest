c81daa539f513c14b105713dc658b34c3f02b462
MilopineDesign <letsgoalreadyyy@gmail.com>
2024-12-19 12:48:15 -0600

    first

---
+++ character.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Character : CharacterBody2D
+{
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
